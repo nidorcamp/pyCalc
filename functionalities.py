56a2839cefa29b7feadfcdc14307b4f7e99f7aa2
borachudo <77072354+borachudo@users.noreply.github.com>
2021-01-08 15:18:45 -0300

    everything in one function

---
+++ functionalities.py
@@ -0,0 +1,10 @@
+def operator(s='+', n1=0, n2=0):
+  if s == '+':
+    tot = n1 + n2
+  if s == '-':
+    tot = n1 - n2:
+  if s == '*':
+    tot = n1 * n2:
+  if s == '/':
+    tot = n1 / n2
+  return tot
