80837d8ac4df45ed65e93d4af0ca8cfb30a609d3
borachudo <77072354+borachudo@users.noreply.github.com>
2021-01-08 15:49:19 -0300

    emplementing the function operator
    this function takes 3 arguments 
    operator(s='+-*/', n1=...-5...5..., n2=...-5...5...)

--- pyCalc.py
+++ pyCalc.py
@@ -2,9 +2,13 @@ import functionalities
 
 msg("pyCalc")
 n1 = int(input("Number: "))
+op = str(input("Operator [+ - * /]: "))
 n2 = int(input("Number: "))
-tot = n1 + n2
+
+tot = operator(s, n1, n2)
+
+
 
 print()
-print(f"{n1} + {n2} = {tot}")
+print(f"{n1} {op} {n2} = {tot}")
 print()
